75b989098497cdc666af447710b8f655c97254f0
Ksyun2142 <67158891+Ksyun2142@users.noreply.github.com>
2021-08-04 10:37:02 +0300

    second commit

--- src/pages/mainmenu.vue
+++ src/pages/mainmenu.vue
@@ -1,23 +1,20 @@
 <template>
-<div>
 <div class="center">
 <table >
     <tr>
-        <td align="center"> <a class="btn-planning main-menu-btn" href="http://192.168.1.81:8080/main"><img src="../assets/images/main/plan_factor_analis.png" width="200"  height="200"><br>ПЛАН-ФАКТНЫЙ АНАЛИЗ<br>ПРОИЗВОДСТВЕННЫХ ЗАТРАТ</a></td>
-        <td align="center"><a href="http://192.168.1.81:8080/main"><img src="../assets/images/main/costs_kultures.png" width="200"  height="200"><br>ДЕТАЛИЗАЦИЯ <br>ЗАТРАТ ПО КУЛЬТУРАМ</a></td>
-        <td align="center"><a href="http://192.168.1.81:8080/main"><img src="../assets/images/main/map.png" width="200"  height="200"><br>КАРТА</a></td>
+        <td align="center"><router-link :to="{ name:  'tree'}"><img src="../assets/images/main/plan_factor_analis.png" width="200"  height="200"><br>ПЛАН-ФАКТНЫЙ АНАЛИЗ<br>ПРОИЗВОДСТВЕННЫХ ЗАТРАТ</router-link></td>
+        <td align="center"><router-link :to="{ name:  'tree'}"><img src="../assets/images/main/costs_kultures.png" width="200"  height="200"><br>ДЕТАЛИЗАЦИЯ <br>ЗАТРАТ ПО КУЛЬТУРАМ</router-link></td>
+        <td align="center"><router-link :to="{ name:  'tree'}"><img src="../assets/images/main/map.png" width="200"  height="200"><br>КАРТА</router-link></td>
     </tr>
     <tr>
-        <td align="center"><a href="http://192.168.1.81:8080/two"><img src="../assets/images/main/tasks_productions.png" width="200"  height="200"> <br>ПРОИЗВОДСТВЕННЫЕ<br>ЗАДАЧИ</a></td>
-        <td align="center"><a href="http://192.168.1.81:8080/one"><img src="../assets/images/main/planning.png" width="200"  height="200"><br>ПЛАНИРОВАНИЕ</a></td>
-         <td align="center"><a href="http://192.168.1.81:8080/main"><img src="../assets/images/main/tr1.png" width="160"  height="160"><br>АГРОГАРАЖ</a></td>
+        <td align="center"><router-link :to="{ name: 'two' }"><img src="../assets/images/main/tasks_productions.png" width="200"  height="200"><br>ПРОИЗВОДСТВЕННЫЕ<br>ЗАДАЧИ</router-link></td>
+        <td align="center"><router-link :to="{ name: 'one' }"> <img src="../assets/images/main/planning.png" width="200"  height="200"><br>ПЛАНИРОВАНИЕ</router-link></td>
+        <td align="center"><router-link :to="{ name:  'tree'}"><img src="../assets/images/main/tr1.png" width="160"  height="160"><br>АГРОГАРАЖ</router-link></td>
     </tr>
     <tr>
-        <td colspan="3" align="center"> <a href="http://192.168.1.81:8080/main"><img src="../assets/images/main/adminBig.png" width="75"  height="75"><br>АДМИНИСТРИРОВАНИЕ</a></td>
+        <td colspan="3" align="center"><router-link :to="{ name:  'tree'}"><img src="../assets/images/main/adminBig.png" width="75"  height="75"><br>АДМИНИСТРИРОВАНИЕ</router-link></td>
     </tr>
 </table>
-
-</div>
 </div>
 </template>
 
